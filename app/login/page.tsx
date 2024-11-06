0bd6bfdd15d62975723a3804497858bdf1d320c0
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-06 09:17:16 -0300

    feat: :sparkles: add a function to check if has user and redirect to dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,53 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Login = () => {
-  return ( 
+const Login = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
+  return (
     <section className="grid grid-cols-2">
       {/* "ESQUERDA" */}
-      <div className="flex flex-col items-center justify-center p-8 ">
-        <div className="mx-auto items-start max-w-[550px]">
-
-        <Image src={"/logo.svg"} alt="Logo" width={150} height={150} className="" />
-        <h1 className="text-4xl font-bold mt-8">Bem-vindo</h1>
-        <p className="text-muted-foreground mt-2 text-base">A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
-        <Button className="w-full mt-8" variant="outline">Entrar com Google <LogInIcon className="w-4 h-4 ml-2" /></Button>
+      <div className="flex flex-col items-center justify-center p-8">
+        <div className="mx-auto max-w-[550px] items-start">
+          <Image
+            src={"/logo.svg"}
+            alt="finance ai"
+            width={173}
+            height={39}
+            className=""
+          />
+          <h1 className="mt-8 text-4xl font-bold">Bem-vindo</h1>
+          <p className="mt-2 text-base text-muted-foreground">
+            A Finance AI é uma plataforma de gestão financeira que utiliza IA
+            para monitorar suas movimentações, e oferecer insights
+            personalizados, facilitando o controle do seu orçamento.
+          </p>
+          <SignInButton>
+            <Button className="mt-8 w-full" variant="outline">
+              <LogInIcon className="ml-2 h-4 w-4" />
+              Entrar com Google
+            </Button>
+          </SignInButton>
         </div>
       </div>
       {/* "DIREITA" */}
-      <Image src="/login.png" alt="Login"  width={1000} height={1000} className="max-h-[100dvh] w-full object-cover pointer-events-none" />
+      <Image
+        src="/login.png"
+        alt="Login"
+        width={1000}
+        height={1000}
+        className="pointer-events-none max-h-[100dvh] w-full object-cover"
+      />
     </section>
   );
-}
+};
 
 export default Login;
