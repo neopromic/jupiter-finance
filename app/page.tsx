8931f21ada7591c7fad8b018cc77ffb64899ebd3
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-06 09:16:29 -0300

    feat: :sparkles: add a function to redirect if user is not logged yet to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-const Home = () => {
-  return ( 
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
     <div>
       <h1>Home</h1>
     </div>
   );
-}
- 
+};
+
 export default Home;
