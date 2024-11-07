88ba359eb6bd9cc5725edbb211d6942eed9581fc
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-07 14:04:52 -0300

    feat: :sparkles: add type-badge component

---
+++ app/(authenticated)/transactions/_components/type-badge.tsx
@@ -0,0 +1,34 @@
+import { Badge } from "@/app/_components/ui/badge";
+import type { Transactions } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transactions;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === "DEPOSIT") {
+    return (
+      <Badge className="bg-primary/10 text-primary hover:bg-primary/10">
+        <CircleIcon size={10} className="mr-2 fill-primary" />
+        Ganho
+      </Badge>
+    );
+  }
+  if (transaction.type === "EXPENSE") {
+    return (
+      <Badge className="bg-danger/10 text-danger hover:bg-danger">
+        <CircleIcon size={10} className="mr-2 fill-danger" />
+        Gasto
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="bg-muted hover:bg-muted">
+      <CircleIcon size={10} className="mr-2 fill-white" />
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
