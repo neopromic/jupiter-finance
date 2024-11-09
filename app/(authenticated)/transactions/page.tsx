bf459532f5beda22d871b29e403f2371781ef7e5
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-08 23:35:47 -0300

    refactor: :recycle: removed old button and use new add transaction button

--- app/(authenticated)/transactions/page.tsx
+++ app/(authenticated)/transactions/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ArrowDownUp } from "lucide-react";
 import React from "react";
 import { DataTable } from "@/app/_components/ui/data-table";
 import { transactionsColumns } from "./_columns";
+import AddTransactionButton from "@/app/_components/add-transaction-button";
 
 const TransactionsPage = async () => {
   const transactions = await db.transactions.findMany({});
@@ -12,10 +11,7 @@ const TransactionsPage = async () => {
     <div className="space-y-6 p-6">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full">
-          Nova transação
-          <ArrowDownUp className="h-4 w-4" />
-        </Button>
+        <AddTransactionButton />
       </div>
       <DataTable columns={transactionsColumns} data={transactions} />
     </div>
