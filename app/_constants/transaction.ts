f9a54f5f72956147f42bc985c37e227845d6ffdf
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-08 23:24:41 -0300

    feat: :recycle: add constants folder to store global datas

---
+++ app/_constants/transaction.ts
@@ -0,0 +1,34 @@
+import { TransactionType } from "@prisma/client";
+
+export const transactionTypeOptions = [
+  {
+    value: TransactionType.EXPENSE,
+    label: "Despesa",
+  },
+  {
+    value: TransactionType.INVESTMENT,
+    label: "Investimento",
+  },
+  {
+    value: TransactionType.DEPOSIT,
+    label: "Depósito",
+  },
+];
+
+export const TRANSACTION_PAYMENT_METHOD_MAP: Record<string, string> = {
+  CREDIT_CARD: "Cartão de crédito",
+  DEBIT_CARD: "Cartão de débito",
+  PIX: "PIX",
+  BANK_TRANSFER: "Transferência bancária",
+};
+
+export const TRANSACTION_CATEGORY_MAP: Record<string, string> = {
+  EDUCATION: "Educação",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  ENTERTAINMENT: "Entretenimento",
+  HOUSING: "Moradia",
+  TRANSPORTATION: "Transporte",
+  SALARY: "Salário",
+  UTILITY: "Utilidade",
+};
