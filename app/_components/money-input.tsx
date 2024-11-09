ede8a125080faa8f5b6145f73e9ca11317c730de
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-08 23:26:34 -0300

    feat: :sparkles: add money input

---
+++ app/_components/money-input.tsx
@@ -0,0 +1,25 @@
+import { NumericFormat, NumericFormatProps } from "react-number-format";
+
+import { Input, InputProps } from "@/app/_components/ui/input";
+import { forwardRef } from "react";
+
+export const MoneyInput = forwardRef(
+  (
+    props: NumericFormatProps<InputProps>,
+    ref: React.ForwardedRef<HTMLInputElement>,
+  ) => {
+    return (
+      <NumericFormat
+        {...props}
+        thousandSeparator="."
+        decimalSeparator=","
+        prefix="R$ "
+        allowNegative={false}
+        customInput={Input}
+        getInputRef={ref}
+      />
+    );
+  },
+);
+
+MoneyInput.displayName = "MoneyInput";
