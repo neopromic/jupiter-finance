1d0f93ed7db87adcc96c85ff42069817c2c176fe
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-22 17:45:02 -0300

    chore(security): :closed_lock_with_key: add env.ts to make dot env not more nullable

---
+++ app/_lib/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+  NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "",
+  NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "",
+  DATABASE_URL: process.env.DATABASE_URL ?? "",
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY ?? "",
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY ?? "",
+};
