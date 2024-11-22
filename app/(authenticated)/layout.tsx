16f32fc952fea1ac608a496b4ca6842cd93fa0ca
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-22 17:51:43 -0300

    feat: :sparkles: add posthog provider to private routes

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../_components/Header";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { PostHogProvider } from "../_lib/providers";
 
 export default async function AuthenticatedLayout({
   children,
@@ -15,9 +16,9 @@ export default async function AuthenticatedLayout({
   }
 
   return (
-    <>
+    <PostHogProvider>
       <Header isEnabled />
       <div className="flex flex-col overflow-hidden">{children}</div>
-    </>
+    </PostHogProvider>
   );
 }
