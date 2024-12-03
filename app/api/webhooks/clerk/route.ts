b64db27b2b5dd3ee3943c26ff3a2dc8dc5b29bc7
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-12-03 15:56:20 -0300

    fix(api route to clerk webhooks): :ambulance: fixed the method name

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,10 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { clerkClient } from "@clerk/nextjs/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
