145dbf87778dabd1915a326f715e467b93afc900
Wesley Souza | Neo <cusaowesley1@gmail.com>
2025-01-06 02:01:23 -0300

    feat: replace main content with HeroLandingPage component for improved landing experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
 import Header from "./_components/Header";
 import HappyNewYearHeader from "./_components/HappyNewYearHeader";
-import GetStartdButton from "./_components/Landing/get-started-button";
+import HeroLandingPage from "./_components/Landing/Hero";
 
 export default function LandingPage() {
   return (
     <>
       <HappyNewYearHeader />
       <Header />
-      <main className="flex flex-1 flex-col items-center justify-center p-6 text-center">
-        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
-          Gerencie suas finanças,
-          <br />
-          Escale sua empresa
-        </h1>
-        <p className="my-6 text-lg text-muted-foreground">
-          Organize suas despesas, clientes, investimentos e ganhos de forma
-          inteligente.
-        </p>
-        <GetStartdButton />
-      </main>
+      <HeroLandingPage />
       <footer className="flex flex-col items-center justify-center p-6 text-center">
         <p className="text-xs text-muted-foreground">
           Feito com 💚 por Wesley Souza
